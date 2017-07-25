a8e9729a57e893e7e1f9443abb90b446f2e280d2
@YeymiGR <yeymigonzalez@gmail.com>
2017-07-25 17:19:57 -0500

    Ejercicio de captura por consola y comparación.

---
+++ src/RelationalOperators.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class RelationalOperators {
+
+
+		public static void main(String[] arg){
+	        
+	        int cont = 0;
+	        
+	        Scanner captura = new Scanner(System.in);
+	        
+	        
+	        
+	        int cant = captura.nextInt();
+	        
+	        while(cont<cant){
+	            int a = captura.nextInt();
+	            int b = captura.nextInt();
+	            
+	            if(a>b)
+	                System.out.println(">");
+	            else if(a<b)
+	                System.out.println("<");
+	            else
+	                System.out.println("=");
+	            cont++;
+	                
+	        }
+	        
+	        
+	        //while 
+	        
+	    }
+
+}
